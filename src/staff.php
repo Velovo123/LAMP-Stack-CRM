28fcb8c25b465a4d92f7a62923a1657c9b8670be
velovo <velovo16@gmail.com>
2024-05-14 19:26:08 +0300

    minor changes

--- src/staff.php
+++ src/staff.php
@@ -61,7 +61,7 @@ include "db_conn.php";
             <td><?php echo $row["title"] ?></td>
             <td>
               <a href="edit.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
-              <a href="delete.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-trash fs-5"></i></a>
+              <a href="delete.php?id=<?php echo $row["id"] ?>" class="link-dark" onclick="return confirmDelete();"><i class="fa-solid fa-trash fs-5"></i></a>
             </td>
           </tr>
         <?php
@@ -75,5 +75,10 @@ include "db_conn.php";
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
+<script>
+    function confirmDelete() {
+      return confirm("Are you sure");
+    }
+</script>
 
 </html>
--- src/studio.php
+++ src/studio.php
@@ -58,8 +58,8 @@ include "db_conn.php";
             <td><?php echo $row["location"] ?></td>
             <td><?php echo $row["capacity"] ?></td>
             <td>
-              <a href="edit-studio.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
-              <a href="delete-studio.php?id=<?php echo $row["id"] ?>" class="link-dark"><i class="fa-solid fa-trash fs-5"></i></a>
+              <a href="edit-studio.php?id=<?php echo $row["id"] ?>" class="link-dark" ><i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
+              <a href="delete-studio.php?id=<?php echo $row["id"] ?>" class="link-dark" onclick="return confirmDelete();"><i class="fa-solid fa-trash fs-5"></i></a>
             </td>
           </tr>
         <?php
@@ -73,5 +73,11 @@ include "db_conn.php";
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
+
+<script>
+    function confirmDelete() {
+      return confirm("Are you sure");
+    }
+</script>
 
 </html>
